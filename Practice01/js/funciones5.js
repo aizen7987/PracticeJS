9f04a54146e708cc82e0c2d32e13d2a642a11a36
Oscar <orene.viera@gmail.com>
2022-10-25 15:23:43 -0600

    Funcion5: conversor de binario a decimal y viceversa

--- Practice01/js/funciones5.js
+++ Practice01/js/funciones5.js
@@ -3,13 +3,42 @@ de base binaria a decimal y viceversa,
 pe. miFuncion(100,2) devolverá 4 base 10.*/
 
 
+const CBinToDec = (valor, base) => {
+    if(valor !== undefined 
+        && typeof valor === 'string'
+        && base === 10){
+            let valorArray = valor.split("");
+            let result=parseInt(valor.substr(0,1));
+            for(let i=0;i<valorArray.length-1;i++){                
+                result=result*2+parseInt(valorArray[i+1]);            
+            }
+            console.info(result);
+    }else 
+    if(valor !== undefined 
+        && typeof valor === 'number'
+        && base === 2){
+            let cadena = "";
+            while(valor>0){
+                (valor%2===0)
+                    ? cadena+="0"
+                    : cadena+="1"
+                valor = parseInt(valor/2)
+            }                
+            console.info(cadena.split("").reverse().join(""));
+    }
+    else {
+        console.info("Valor o base no definidos");
+    }
+    
+}
+CBinToDec(4,2);
 
-
-
+// 10101
 /*16) Programa una función que devuelva el monto 
 final después de aplicar un descuento a una 
 cantidad dada, pe. miFuncion(1000, 20) 
 devolverá 800.*/
+
 
 
 const Descontar = (monto = 0, descuento = 0) =>
