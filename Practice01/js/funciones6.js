3e90178cae86014d639603d1de1cfc90803a6b33
Ernesto Afane <ernesto@builtmighty.com>
2022-10-31 19:37:03 -0600

    Ejercicio 18 contar vocales y consonantes

--- Practice01/js/funciones6.js
+++ Practice01/js/funciones6.js
@@ -1,10 +1,27 @@
 /*18) Programa una función que dada una cadena de texto 
-cuente el número de vocales y consonantes, 
+cuente el número de vocales y consonantes, pe. miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5.*/
 
 
+const contarLetras = (cadena,tipo) =>{
+    if(typeof tipo !== 'string')
+        return;
 
+    if(tipo==='V'){
+        (cadena!==undefined)
+            ? console.info(cadena.match(/[aeiou]/gi).length)
+            : console.warn('Cadena no valida');
+    }
 
-pe. miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5.*/
+    if(tipo==='C'){
+        (cadena!==undefined)
+            ? console.info(cadena.match(/[bcdfghjklnmpqrstvwxyz]/gi).length)
+            : console.warn('Cadena no valida');
+    }
+    
+}
+
+contarLetras("abngeebbni","C");
+
 /*19) Programa una función que valide que un 
 texto sea un nombre válido, pe. miFuncion("Jonathan MirCha") 
 devolverá verdadero.*/
